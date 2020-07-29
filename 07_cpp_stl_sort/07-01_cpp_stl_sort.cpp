f7cee7470d4d1fc728086f45a41c661b5273cde0
Keriel <keriel@jupiterflow.com>
2020-07-29 19:43:46 +0900

    :pencil: C++ STL sort() 파헤치기 커밋

---
+++ 07_cpp_stl_sort/07-01_cpp_stl_sort.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a[10] = { 9, 3, 5, 4, 1, 10, 8, 6, 7, 2 };
+    sort(a, a + 10);
+
+    for (int i = 0; i < 10; ++i) {
+        cout << a[i] << ' ';
+    }
+
+    return 0;
+}
